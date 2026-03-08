93fe403cb84d801f010143e8deb1d216234a685f
snwflwrsoph <asophia.peraltar@gmail.com>
2026-03-07 20:34:10 -0700

    feat: create and connect database

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {Express, Request, Response} from 'express';
 import morgan from 'morgan';
 import apiRouter from './src/routes';
+import connectionDB from './src/connection/connection';
 
 const app: Express = express(); 
 const port: number = 3000; 
@@ -8,6 +9,8 @@ const port: number = 3000;
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(apiRouter);
+
+connectionDB();
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
@@ -31,3 +34,4 @@ app.listen(port, () => {
 })
 
 */
+
